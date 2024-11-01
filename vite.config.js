1b03abdfc2be6b8b9a812f80b2d7184e79269abf
xushuo <isxu.shuo@foxmail.com>
2024-11-01 11:26:16 +0800

    elementPlus按需引入配置

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,22 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+// 导入element plus
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    // 注册element plus
+    // ...
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {
