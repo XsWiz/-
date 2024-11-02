3c466e2e91bc80d82f4ed698d2bdf8f38f861eb7
xushuo <isxu.shuo@foxmail.com>
2024-11-02 11:20:42 +0800

    静态资源创建和引入

--- src/main.js
+++ src/main.js
@@ -4,13 +4,16 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+/* 2 路由引入 */
 import router from './router'
-/* 在入口文件 接口测试 */
+/*1 在入口文件 接口测试 */
 import { getCategory } from '@/apis/testAPI'
 getCategory().then(res => {
   console.log(res)
 })
-/* 在入口文件 接口测试 */
+/* 1 在入口文件 接口测试 */
+/* 3 静态资源样式 样式重置 */
+import  '@/styles/common.scss'
 const app = createApp(App)
 
 app.use(createPinia())
