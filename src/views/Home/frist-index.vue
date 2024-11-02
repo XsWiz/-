72a4c3eb0011711510196663526056569f47008b
xushuo <isxu.shuo@foxmail.com>
2024-11-02 19:05:09 +0800

    插槽封装

--- src/views/Home/frist-index.vue
+++ src/views/Home/frist-index.vue
@@ -4,7 +4,8 @@ import HomeCategory from './components/HomeCategory.vue'
 import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
-import homeProduct from './components/HomeProduct.vue'
+import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -14,6 +15,16 @@ import homeProduct from './components/HomeProduct.vue'
   </div>
   <HomeNew />
   <HomeHot />
-  <homeProduct />
+  <HomeProduct />
+  <HomePanel title="新鲜好物" subTitle="新鲜好物">
+    <div>
+      新鲜好物的插槽
+    </div>
+  </HomePanel>
+  <HomePanel title="人气推荐" subTitle="新鲜好物">
+    <div>
+      人气推荐的插槽
+    </div>
+  </HomePanel>
 </template>
 
