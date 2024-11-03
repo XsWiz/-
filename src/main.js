f39582e3de113cfda7a65947eb80c446687d7e56
xushuo <isxu.shuo@foxmail.com>
2024-11-03 09:40:41 +0800

    图片懒加载实现

--- src/main.js
+++ src/main.js
@@ -1,21 +1,32 @@
-// import './assets/main.css'   静态资源样式 样式重置
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
-/* 2 路由引入 */
 import router from './router'
-/*1 在入口文件 接口测试 */
-// import { getCategory } from '@/apis/testAPI'
-// getCategory().then(res => {
-//   console.log(res)
-// })
-/* 1 在入口文件 接口测试 */
-/* 3 静态资源样式 样式重置 */
-import  '@/styles/common.scss'
+import '@/styles/common.scss'
+// 1 注册全局指令
+// 2 导入useIntersectionObserver  vueuse
+import { useIntersectionObserver } from '@vueuse/core'
 const app = createApp(App)
 
+app.directive('img-lazy', {
+  /* ... */
+  mounted(el, binding) {
+    //1 el 指令绑定的dom对象
+    //binding  binding: binding.value  指令等于号后面绑定的表达式的值  图片url
+    console.log('111', el, '222', binding.value)
+    // 2
+     useIntersectionObserver(
+      el,
+      ([{ isIntersecting }]) => {
+        console.log(isIntersecting)
+        // 3
+        if (isIntersecting)
+          el.src = binding.value
+      },
+    )
+  },
+})
 app.use(createPinia())
 app.use(router)
 
