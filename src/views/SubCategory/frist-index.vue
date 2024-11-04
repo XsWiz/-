c14c888b1bad236d0a2b2636938c4a8b6c5da6aa
xushuo <isxu.shuo@foxmail.com>
2024-11-04 19:44:08 +0800

    二级分类-tab栏切换

--- src/views/SubCategory/frist-index.vue
+++ src/views/SubCategory/frist-index.vue
@@ -30,6 +30,12 @@ const getGoodList = async () => {
 onMounted(() => {
   getGoodList()
 })
+/* tab切换 */
+const tabChange = () => {
+  console.log('tab切换了', reqData.value.sortField)
+   reqData.value.page = 1
+  getGoodList()
+}
 </script>
 
 <template>
@@ -44,7 +50,7 @@ onMounted(() => {
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField"  @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
