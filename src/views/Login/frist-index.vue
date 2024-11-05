e7d0597673049d98413aaaff4c5740e4a200f48f
xushuo <isxu.shuo@foxmail.com>
2024-11-05 20:45:23 +0800

    登录-表单校验-整体校验

--- src/views/Login/frist-index.vue
+++ src/views/Login/frist-index.vue
@@ -30,6 +30,19 @@ const rules = {
   ]
 
 }
+// 3 获取form实例做统一校验
+const ruleFormRef = ref(null)
+const doLogin = () => {
+  // 调用方法
+  ruleFormRef.value.validate((valid) => {
+    // valid: 所有表单都通过校验  才为true
+    console.log(valid)
+    // 以valid做为判断条件 如果通过校验才执行登录逻辑
+    if (valid) {
+        //
+       }
+   })
+}
 </script>
 
 
@@ -54,7 +67,7 @@ const rules = {
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form :model="form" :rules="rules" label-position="right" label-width="60px"
+            <el-form  ref="ruleFormRef" :model="form" :rules="rules" label-position="right" label-width="60px"
               status-icon>
               <el-form-item prop="account" label="账户">
                 <el-input v-model="form.account"  />
@@ -67,7 +80,7 @@ const rules = {
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" @click="doLogin" class="subBtn">点击登录</el-button>
             </el-form>
           </div>
         </div>
