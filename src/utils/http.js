90b26d6505d4c5c7c6b750cc19da24616ce7ae47
xushuo <isxu.shuo@foxmail.com>
2024-11-06 16:07:39 +0800

    登录-登录请求拦截器token

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,6 +3,8 @@ import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 // axios基础封装
 import axios from "axios";
+// 获取pinia的token数据
+import { useUserStore } from '@/stores/user.js'
 const httpInstance=axios.create({
   // 接口基地址
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -13,6 +15,13 @@ const httpInstance=axios.create({
 
 /* axios请求拦截器 */
 httpInstance.interceptors.request.use(config => {
+  // 1. 从pinia获取token数据
+  const userStore = useUserStore()
+  // 2. 按照后端的要求拼接token数据
+  const token = userStore.userInfo.token
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
   return config
 }, e => Promise.reject(e))
 
